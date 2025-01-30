b9b1fbad81a72f42e1911875c427478cb04cc862
NguyenDK2247 <neyugndongkhoibar@gmail.com>
2025-01-30 16:15:01 +0200

    MagnifyingLens working

--- XR again/Assets/Magnifying Glass/MagnifyingLens.cs
+++ XR again/Assets/Magnifying Glass/MagnifyingLens.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MagnifyingLens : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform playerCamera; // Reference to the player's main camera
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PositionLensCamera();
+    }
+
+    private void PositionLensCamera()
+    {
+        Vector3 directionVector = transform.position - playerCamera.position;
+        // Position the lens camera in front of the player's camera
+        Vector3 lensPosition = transform.position + directionVector;
+        transform.LookAt(lensPosition, transform.up);
     }
 }
